d7e1fdb20f810fcff641de4b08f6766d7dd2527d
Romain Vuillemot <romain.vuillemot@gmail.com>
2022-08-24 00:19:27 +0200

    adding nages

--- neptunedata/models/models.py
+++ neptunedata/models/models.py
@@ -14,5 +14,19 @@ class Sexes:
             var_sexes = json.load(d)
             return var_sexes
 
+class Nages:
+
+    def __init__(self):
+        self.get_nages()
+
+    def get_nages():
+        path_nages = os.path.join(os.path.dirname(__file__), 'nages.json')
+        var_nages = {}
+
+        with open(path_nages) as d:
+            var_nages = json.load(d)
+            return var_nages
+
 if __name__ == '__main__':
     print(Sexes.get_sexes())
+    print(Nages.get_nages())
